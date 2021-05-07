e115fa1ccd9141bfc0a9ce54b0043527140da5fa
student <s1081692@gm.pu.edu.tw>
2021-05-07 15:59:38 +0800

    Initial commit

---
+++ app/src/main/java/tw/edu/pu/csim/s1081692/explicitintent/SecondActivity.kt
@@ -0,0 +1,11 @@
+package tw.edu.pu.csim.s1081692.explicitintent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
